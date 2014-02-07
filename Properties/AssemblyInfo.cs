8728908d921f7e3fd6c74813b5f85ae014f3aa60
izrik <izrik@izrik.com>
2014-02-07 10:39:17 -0600

    Initial project structure.

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Voyage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Metaphysics Industries, Inc.")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("2014 Metaphysics Industries, Inc.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.*")]
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
